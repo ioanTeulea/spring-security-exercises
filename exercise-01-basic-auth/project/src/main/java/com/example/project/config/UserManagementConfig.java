bd576b094af595852d3464ba4d5b084153bbf59d
Ioan Teulea <ioanteulea@gmail.com>
2026-03-19 13:59:59 +0200

    feat: implement basic authentication and authorization (PoC)
    - Configured UserDetailsService using InMemoryUserDetailsManager.
    
    - Set PasswordEncoder to NoOpPasswordEncoder (plain text) for PoC.
    
    - Restricted endpoint access to authenticated users only via authorizeHttpRequests.

---
+++ exercise-01-basic-auth/project/src/main/java/com/example/project/config/UserManagementConfig.java
@@ -0,0 +1,26 @@
+package com.example.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserManagementConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
