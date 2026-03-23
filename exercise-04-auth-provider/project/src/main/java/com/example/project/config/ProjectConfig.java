e802fd1f961c3cc09ab6a03d2575a91184f5dec7
Ioan Teulea <ioanteulea@gmail.com>
2026-03-23 19:20:47 +0200

    feat: implement custom JDBC AuthenticationProvider and multi-layered security filter chain

--- exercise-04-auth-provider/project/src/main/java/com/example/project/config/ProjectConfig.java
+++ exercise-04-auth-provider/project/src/main/java/com/example/project/config/ProjectConfig.java
@@ -1,4 +1,51 @@
 package com.example.project.config;
 
+import com.example.project.authProviders.CustomAuthenticationProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery =
+                "select username, password, enabled from users where username = ?";
+        String authsByUserQuery =
+                "select username, authority from spring.authorities where username = ?";
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        return new CustomAuthenticationProvider(userDetailsService, passwordEncoder);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,AuthenticationProvider authenticationProvider) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(authenticationProvider);
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+        return http.build();
+    }
 }
